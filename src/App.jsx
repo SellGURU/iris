23b114d652b357dbb676a41a2eca1700382c671e
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-06-10 19:00:33 +0330

    fixed route 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router-dom";
 function App() {
   return (
     <>
-      <HashRouter basename="/test">
+      <HashRouter basename="/">
         <AppRoutes />
       </HashRouter>
     </>
--- src/page/faceMash.jsx
+++ src/page/faceMash.jsx
@@ -396,7 +396,7 @@ const FaceMesh = () => {
   return (
     <>
       <button
-        className="px-3 py-2 bg-red-600 text-white mb-10 rounded-md"
+        className="px-3 py-2 absolute top-10 left-10 bg-red-600 text-white mb-10 rounded-md"
         onClick={() => saveAccess("false")}
       >
         logout
