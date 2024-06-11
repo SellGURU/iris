94982b1f83435011dd4e4feafc063fa2a38bc1e7
amir <amirdehghani137722@gmail.com>
2024-06-11 19:18:58 +0330

    add login 2

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,9 @@
 import FaceMesh from "./page/faceMash";
 import Login from "./page/login";
+import Login2 from './page/Login2';
 
 export const route = [
   { path: "", element: <FaceMesh /> },
   { path: "/login", element: <Login /> },
+  { path: "/login2", element: <Login2 /> },
 ];
