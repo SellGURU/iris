1c936525feb796c4c9086674cf4750281cb3a6b2
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-06-13 18:36:21 +0330

    UPDATE:add getnumber|getnumber page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,19 @@
 import FaceMesh from "./page/faceMash";
-import Login from "./page/login";
+import SignUp from "./page/signup";
+import GetCode from "./page/signup/getCode";
+import GetNumberPage from "./page/signup/getNumberPage";
+import WelcomePage from "./page/signup/welcome";
 
 export const route = [
   { path: "", element: <FaceMesh /> },
-  { path: "/login", element: <Login /> },
+  {
+    path: "/SignUp",
+    element: <SignUp />,
+    children: [
+      { path: "", element: <WelcomePage /> },
+      { path: "getnumber", element: <GetNumberPage /> },
+      { path: "getnumber", element: <GetCode /> },
+      { path: "test", element: <h1>test</h1> },
+    ],
+  },
 ];
