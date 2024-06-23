f763bcbfd29490409c8cbfb18be9e475c14256cd
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-06-23 11:56:42 +0330

    FIXED:redirect when token is not valid

--- src/utility/isLogin.jsx
+++ src/utility/isLogin.jsx
@@ -1,12 +1,12 @@
 import {useLocalStorage} from "@uidotdev/usehooks";
 import {Navigate, useNavigate} from "react-router-dom";
+import {toast} from "react-toastify";
+import {useState} from "react";
 
 export const IsLogin = ({children}) => {
     const navigate = useNavigate();
-
-    const [access,] = useLocalStorage("token")
-    console.log(!access)
-    if (!access) {
+    const validToken =checkValidToken()
+    if (!validToken) {
         return (
             <Navigate to={"/login"}/>
         )
@@ -14,4 +14,23 @@ export const IsLogin = ({children}) => {
     return (
         <>{children}</>
     )
+}
+const checkValidToken=()=>{
+    const [access,] = useLocalStorage("token")
+
+    const xhr = new XMLHttpRequest();
+        let response=true
+        xhr.open('POST','https://iris.ainexus.com/api/v1/analyze', true);
+        xhr.onload = function (e) {
+            // console.log(e)
+             response=JSON.parse(e.target.status)
+        }
+        let fileData = new FormData();
+        fileData.append('error_threshold', 10);
+        fileData.append('gender', 'masculine');
+        fileData.append('frontal_current', "");
+        xhr.setRequestHeader('Authorization', 'Bearer ' +access)
+        xhr.send(fileData);
+        return response
+
 }
