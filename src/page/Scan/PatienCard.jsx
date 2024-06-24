14a30bfc00c8ea8f2ba7b44b661489ceb796f021
SmurfsNIma <sadeghinima090@gmail.com>
2024-06-24 15:27:37 +0330

    fix styles

--- src/page/Scan/PatienCard.jsx
+++ src/page/Scan/PatienCard.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import ButtonSecondary from "../../components/button/buttonSecondary";
 import ButtonPrimary from "../../components/button/buttonPrimery";
 
-export const PatienCard = () => {
+export const PatienCard = ({index , patient}) => {
+    const {id, date , comment} = patient
   return (
     <div className="flex gap-12 items-center justify-start shadow-lg border px-4 pt-2">
       <div className="flex items-start self-start gap-5 ">
-        1
+        {index}
         <img className="mt-3" src="/public/Rectangle 18044.svg" alt="" />
       </div>
       <div className="w-full flex flex-col items-start  justify-center ">
         <div className="flex justify-between w-full gap-8 border-b py-3">
           <h2 className="text-xl font-bold text-[#1A1919]">
-            Patient Id: 11223344
+            Patient Id: {id}
           </h2>
           <div className="flex items-center gap-1 text-[#544BF0] font-medium text-base mr-24 ">
-            Show Comments(0)
+            Show Comments ({comment})
             <img src="/public/arrow-up.svg" alt="" />
           </div>
           <div className="flex gap-4 items-center">
             <div className="font-medium text-[#606060]">
-              Last Scan: 12 April 2024{" "}
+              Last Scan: {date}
             </div>
             <div className="flex items-center gap-1  text-[#544BF0]">
               <img src="/public/fi_plus-blue.svg" alt="" />
@@ -62,7 +63,7 @@ export const PatienCard = () => {
             </div>
           </div>
         </div>
-         <div className=" mt-5 w-full border-t py-3 flex justify-between items-center">
+         <div className={` mt-5 w-full border-t py-3 flex justify-between items-center ${!comment ? 'hidden' : '' }`}>
         <h3 className="text-xl font-medium text-[#1a1919]">Comments:</h3>
         <p> 12 April 2024  Lorem Ipsum is simply dummy text of the printing and typesetting industry... <span className="text-[#544BF0] underline">Add Comment</span></p>
         <div className="flex items-center gap-1 text-[#544BF0]">
