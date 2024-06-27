d8154747d6ec266f6c2562ce97e007bb083fb89b
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-06-27 09:44:38 +0330

    UPDATE:search patient in scan components

--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export const SearchBox = ({placeHolder}) => {
+export const SearchBox = ({placeHolder,changeHandler}) => {
   return (
     <div className='min-w-[432px] rounded-lg px-6 py-3 shadow-lg outline-none border flex gap-2 items-center '>
         <img src="search-normal.svg" alt="" />
-        <input placeholder={placeHolder} type="text" className="text-[#444444] w-full ring-0 focus:ring-0 focus:outline-none bg-transparent outline-none border-none" />
+        <input onChange={changeHandler} placeholder={placeHolder} type="text" className="text-[#444444] w-full ring-0 focus:ring-0 focus:outline-none bg-transparent outline-none border-none" />
     </div>
   )
 }
