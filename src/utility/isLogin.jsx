46fec636841890d18e9493a42ea44bf99af1d406
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-07-10 19:42:06 +0330

    FIXED:login fixed

--- src/utility/isLogin.jsx
+++ src/utility/isLogin.jsx
@@ -6,8 +6,8 @@ import {useState} from "react";
 export const IsLogin = ({children}) => {
     const navigate = useNavigate();
     const validToken = checkValidToken()
-    console.log(validToken)
-    if (validToken) {
+    // console.log(validToken)
+    if (validToken ) {
         return (
             <Navigate to={"/login"}/>
         )
@@ -22,33 +22,40 @@ const checkValidToken = () => {
     const [status, setStatus] = useState(true)
     const xhr = new XMLHttpRequest();
     let response = true;
-    xhr.open('POST', 'https://iris.ainexus.com/api/v1/analyze', true);
+    try {
+        xhr.open('POST', 'https://iris.ainexus.com/api/v1/analyze', true);
 
-    xhr.onload = function (e) {
-        if (xhr.status === 401) {
-            setStatus(xhr.status)
-            setIsPanding(false)
-        } else {
-            // console.error('okey', xhr.status);
-            setIsPanding(false)
+        xhr.onload = function (e) {
+            if (xhr.status === 401 || xhr.status === 403) {
+                setStatus(xhr.status)
+                setIsPanding(false)
+            } else {
+                // console.error('okey', xhr.status);
+                setIsPanding(false)
 
+            }
+        };
+
+        xhr.onerror = function () {
+            console.log('Request failed');
+        };
+
+
+        let fileData = new FormData();
+        fileData.append('error_threshold', 10);
+        fileData.append('gender', 'masculine');
+        fileData.append('frontal_current', "");
+        xhr.setRequestHeader('Authorization', 'Bearer ' + access);
+        xhr.send(fileData);
+        // TODO: fix the Condition
+        if (!isPanding) {
+            // console.log(status)
+            console.log(isPanding)
+            return (status == 401 || status == 403 );
         }
-    };
-
-    xhr.onerror = function () {
-        console.error('Request failed');
-    };
-
-    let fileData = new FormData();
-    fileData.append('error_threshold', 10);
-    fileData.append('gender', 'masculine');
-    fileData.append('frontal_current', "");
-    xhr.setRequestHeader('Authorization', 'Bearer ' + access);
-    xhr.send(fileData);
-    // TODO: fix the Condition
-    if (!isPanding) {
-        console.log(status)
-        return (status === 401 || status === 403)
+    }catch (e){
+        console.log("error")
     }
+
 
 }
