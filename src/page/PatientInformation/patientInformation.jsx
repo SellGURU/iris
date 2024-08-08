514b6a3e5e151974ba463caafec51bb545386504
amir <amirdehghani137722@gmail.com>
2024-08-08 13:45:00 +0330

    fix some issue

--- src/page/PatientInformation/patientInformation.jsx
+++ src/page/PatientInformation/patientInformation.jsx
@@ -11,8 +11,13 @@ import { Button } from "symphony-ui";
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
-export const PatientInformation = () => {
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
+export const PatientInformation = () => {
+    const getRand = () => {
+        return Math.floor(Math.random() * (100000 - 1000)) + 100
+    }
     const navigate = useNavigate();
     const [gender, setGender] = useState("masculine");
     const [threhold, setthrehold] = useState(10)
@@ -32,6 +37,21 @@ export const PatientInformation = () => {
     const [isShowTour,] = useLocalStorage("tour")
     const [showMore,setShowMore] = useState(false)
     const {register, getValues, handleSubmit} = useForm()
+    const formik = useFormik({
+        initialValues:{
+            id:getRand(),
+            firstName:'',
+            lastName:'',
+            email:'',
+            phone:'',
+            gender:''
+        },
+        validationSchema:Yup.object().shape({
+            firstName:Yup.string().required(),
+            lastName:Yup.string().required(),
+            email:Yup.string().email()
+        })
+    })
 
     // submit the data to start the face mash
     const onSubmitData = (data, e) => {
@@ -57,9 +77,7 @@ export const PatientInformation = () => {
             navigate("/faceCamera")
         }
     }
-    const getRand = () => {
-        return Math.floor(Math.random() * (100000 - 1000)) + 100
-    }
+
     return (
         <>
         <img className="h-full md:w-full fixed z-0 left-0 top-0 md:top-32" src="./Vector.svg" alt="" />
@@ -76,48 +94,48 @@ export const PatientInformation = () => {
             <h1 className={"text-2xl font-medium"}>Client Information</h1>
             <p className={"w-[450px] sm:w-[600px] md:w-[720px] text-[18px] 2xl:text-xl font-[14px] text-center"}>The client ID is unique, and if a duplicate ID is entered, the scan will be added to the history of that record. All fields are mandatory to fill out.</p>
             <form  onSubmit={handleSubmit(onSubmitData)}>
-                <div className={"flex 2xl:grid xl:grid-cols-2 relative items-center justify-center flex-col gap-5"}>
-                    <CardPatient className={"w-[550px] md:w-[600px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
-                        <div className="flex w-full justify-between items-center">
-                            <h1 className={"w-full md:w-[500px] text-[18px] font-medium"}>Client ID</h1>
-                            {/* <input disabled defaultValue={getRand()} {...register("id")} className={"border-b outline-none h-10 w-full "}
-                                placeholder={"Enter Patient ID"}/> */}
-                            <div className="w-full text-[#7E7E7E] text-[18px]">{getRand()}</div>
+                <div className={"flex lg:grid lg:grid-cols-2 relative items-center justify-center flex-col gap-5"}>
+                    <CardPatient className={"w-[550px] order-1 md:w-[600px] lg:w-[480px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
+                        <div className="flex lg:grid xl:flex w-full justify-between items-center">
+                            <h1 className={"w-full md:w-[500px] lg:w-[300px] lg:[500px] text-[18px] font-medium"}>Client ID</h1>
+                            <input disabled {...formik.getFieldProps("id")} className={"border-b outline-none h-10 w-full "}
+                                placeholder={"Enter Patient ID"}/>
+                            {/* <div className="w-full text-[#7E7E7E] text-[18px]">{getRand()}</div> */}
                         </div>
                     </CardPatient>
-                    <CardPatient className={"w-[550px] md:w-[600px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
+                    <CardPatient className={"w-[550px] order-2 md:w-[600px] lg:w-[480px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
                         <div className="flex w-full justify-between items-center">
-                            <h1 className={"w-full md:w-[500px] text-[18px] font-medium"}>First Name</h1>
-                            <input  {...register("id")} className={"border-b outline-none h-10 w-full "}
+                            <h1 className={"w-full md:w-[500px] lg:w-[300px] lg:[500px] text-[18px] font-medium"}>First Name</h1>
+                            <input  {...formik.getFieldProps("firstName")} className={"border-b outline-none h-10 w-full "}
                                 placeholder={"Enter First Name"}/>
 
                         </div>
                     </CardPatient>
-                    <CardPatient className={"w-[550px] md:w-[600px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
+                    <CardPatient className={"w-[550px] order-2 md:w-[600px] lg:w-[480px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
                         <div className="flex w-full justify-between items-center">
                             <h1 className={"w-full md:w-[500px] text-[18px] font-medium"}>Last Name</h1>
-                            <input  {...register("id")} className={"border-b outline-none h-10 w-full "}
+                            <input {...formik.getFieldProps("lastName")} className={"border-b outline-none h-10 w-full "}
                                 placeholder={"Enter Last Name"}/>
 
                         </div>
                     </CardPatient>
-                    <CardPatient className={"w-[550px] md:w-[600px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
+                    <CardPatient className={"w-[550px] order-2 md:w-[600px] lg:w-[480px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
                         <div className="flex w-full justify-between items-center">
                             <h1 className={"w-full md:w-[500px] text-[18px] font-medium"}>E-Mail <span className="text-[#444444] mr-1 font-[400] opacity-50">(Optional)</span></h1>
-                            <input  {...register("id")} className={"border-b outline-none h-10 w-full "}
+                            <input type="email" {...formik.getFieldProps("email")} className={"border-b outline-none h-10 w-full "}
                                 placeholder={"Enter E-Mail"}/>
 
                         </div>
                     </CardPatient>      
-                    <CardPatient className={"w-[550px] md:w-[600px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
+                    <CardPatient className={"w-[550px] order-2 md:w-[600px] lg:w-[480px] 2xl:w-[550px] bg-white z-20 h-[105px] md:h-[88px] border"}>
                         <div className="flex w-full justify-between items-center">
                             <h1 className={"w-full md:w-[500px] text-[18px] font-medium"}>Phone<span className="text-[#444444] font-[400] opacity-50 ml-1">(Optional)</span></h1>
-                            <input  className={"border-b outline-none h-10 w-full "}
+                            <input type="number" {...formik.getFieldProps("phone")} className={"border-b outline-none h-10 w-full "}
                                 placeholder={"Enter Phone"}/>
 
                         </div>
                     </CardPatient>                                                            
-                    <CardPatient className={`w-[550px] md:w-[600px] 2xl:w-[550px] bg-white  border h-[88px] `}>
+                    <CardPatient className={` w-[550px]  md:w-[600px] lg:w-[480px] 2xl:w-[550px]  order-6 lg:order-1 2xl:order-1 bg-white  border h-[88px] `}>
                         <div className="flex w-full justify-between items-center">
                             <div className="flex cursor-pointer justify-start items-center w-[500px]">
                                 <h1 className={" text-[18px]  font-medium"}>Facial Esthetic Preference </h1>
@@ -141,8 +159,18 @@ export const PatientInformation = () => {
                            placeholder={"number"}/>
 
                 </CardPatient> */}
-                <div className="mt-2 2xl:flex 2xl:justify-center 2xl:mt-8">
-                    <Button type="submit" theme="iris-large">
+                <div className="mt-2 lg:flex lg:justify-center lg:mt-8 xl:mt-2 xl:block 2xl:flex 2xl:justify-center 2xl:mt-8">
+                    <Button onClick={() => {
+                        setSex(gender)
+                        setPatientID(formik.id)
+                        setErrorThreshold(threhold)
+
+                        if (isShowTour) {
+                            navigate("/tour")
+                        } else {
+                            navigate("/faceCamera")
+                        }                        
+                    }} disabled={!formik.isValid || !formik.touched.firstName} type="submit" theme="iris-large">
                         Save & Continue
                     </Button>
 
