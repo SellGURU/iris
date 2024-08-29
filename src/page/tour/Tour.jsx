8d0b260b11de72b9d1293171ffb4778d9def33c2
amir <amirdehghani137722@gmail.com>
2024-08-29 13:58:38 +0330

    update tour

--- src/page/tour/Tour.jsx
+++ src/page/tour/Tour.jsx
@@ -32,7 +32,7 @@ export const Tour = () => {
                 return (
                     <StepInstructions
                         step="1"
-                        text=" Click 'Start Scan' to open your camera and capture a well-lit and clear image, or use 'Upload Image' to select a recent, centered photo from your gallery."
+                        text="Click 'Start Scan' to open your camera and capture a well-lit and clear image, or use 'Upload Image' to select a recent, photo from your gallery."
                         image={'image/tour-1.svg'}
                         note="Note: Remove glasses, ensure the subject's face is clear of hair, use a neutral background, and maintain good lighting for optimal results."
                         onNext={handleNext}
@@ -76,7 +76,7 @@ export const Tour = () => {
                         step="4"
                         text="Once all poses are completed successfully, press the 'Finish' button to begin processing and preparing the report."
                         image={'image/tour4.png'}
-                        note="Note: You can also perform a new scan, and all records will be saved in your scan libary."
+                        note="You can also perform a new scan, and all records will be saved in your scan library."
                         onNext={() => {
                             setIsShowTour(false)
                             navigate("/facecamera")
