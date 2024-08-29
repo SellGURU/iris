ad44ebb7cd1910c4274b41ecde5e6442c854579d
amir <amirdehghani137722@gmail.com>
2024-08-29 14:07:24 +0330

    fix text

--- src/page/faceMash.jsx
+++ src/page/faceMash.jsx
@@ -814,7 +814,7 @@ const FaceMesh = () => {
                     <h1 className={"text-[28px] mt-[0px] lg:mt-[-60px] font-medium"}>Face Scanner</h1>
                     <p className={"text-[18px] max-w-[830px] xl:max-w-full px-[24px] text-center font-normal"}>Please provide scans of your face from the left, right, and front to ensure a complete analysis.<span onClick={() => {
                         navigate('/tour')
-                    }} className="text-primary-color cursor-pointer"> How to scan face?</span></p>
+                    }} className="text-primary-color cursor-pointer"> How to scan a face?</span></p>
 
                                 {!isCameraStart &&
                                     <div className="flex md:hidden justify-center items-center">
