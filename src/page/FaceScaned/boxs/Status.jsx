e9c2681003b02a56c3d12aed1ceb413cfd48a507
amir <amirdehghani137722@gmail.com>
2024-12-01 15:48:17 +0330

    fix status issuees

--- src/page/FaceScaned/boxs/Status.jsx
+++ src/page/FaceScaned/boxs/Status.jsx
@@ -18,21 +18,28 @@ const Status =({status,isFull}) => {
                         <div className="w-1/3 h-full bg-[#03DAC5]" style={{backgroundColor:'#03DAC5'}}></div>
                         <div className="w-1/3 h-full bg-primary-color" style={{backgroundColor:'#544BF0'}}></div>
                     </div>
-                    <div className={`absolute flex items-end gap-1 top-[-24px] ${resolvePosition()} `} style={{top:'-24px'}}>
+                    <div className={`absolute flex gap-1 top-[-24px] ${resolvePosition()} `} style={{top:'-18px'}}>
 
                         {status == 'Action Needed' ?
                         <>
                             <img className="" src="./icons/statusBarFlip.svg" alt="" />
-                            <div className="text-[10px] font-light self-center">
+                            <div className="text-[10px] mt-[-14px] font-light self-center" style={{marginTop:'-14px'}}>
                                 {status}
-                            </div>                           
+                            </div>        
+                            <div className="w-4 h-4 absolute bottom-[-16px] flex justify-center items-center left-[-6px] border border-black rounded-full" style={{bottom:'-16px',left:'-6px'}}>
+                                <div className="w-2 h-2 bg-black rounded-full"></div>
+                            </div>                                               
                         </>
                         :
                         <>
-                            <div className="text-[10px] font-light self-center">
+                            <div className="text-[10px] mt-[-14px] font-light self-center"  style={{marginTop:'-14px'}}>
                                 {status}
-                            </div>                        
+                            </div>     
+                  
                             <img src="./icons/StatusBar.svg" alt="" />
+                            <div className="w-4 h-4 absolute bottom-[-16px] flex justify-center items-center right-[-6px] border border-black rounded-full" style={{bottom:'-16px',right:'-6px'}}>
+                                <div className="w-2 h-2 bg-black rounded-full"></div>
+                            </div>
                         </>
                         }
                     </div>
