ad3eaf11e370e99c6bbc968aa9cbabc4216b629f
amir <amirdehghani137722@gmail.com>
2024-12-02 21:02:31 +0330

    update key

--- src/help.js
+++ src/help.js
@@ -12,17 +12,25 @@ const useConstructor = (callBack = () => {}) => {
 };
 
   const encryptTextResolver= (text) => {
-    console.log(text)
-    const secretKey = 'amir'
-    // return CryptoJS.AES.encrypt(text.trim(), secretKey).toString();
-    const iv = CryptoJS.lib.WordArray.random(16); // Generate random IV
-    const encrypted = CryptoJS.AES.encrypt(text, CryptoJS.enc.Utf8.parse(secretKey), {
+    const secretKey ='Hello12345678910'
+    const key = CryptoJS.enc.Utf8.parse(secretKey.slice(0, 16));  // 16-byte key (AES-128)
+    
+    // Generate a random 16-byte IV
+    const iv = CryptoJS.lib.WordArray.random(16);
+    
+    // Encrypt the text using AES-CBC
+    const encrypted = CryptoJS.AES.encrypt(text, key, {
       iv: iv,
       mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.Pkcs7,
+      padding: CryptoJS.pad.Pkcs7
     });
-    // Return the IV and ciphertext concatenated
-    return iv.toString(CryptoJS.enc.Base64) + ':' + encrypted.toString();
+
+    // Base64 encode the IV and ciphertext
+    const encryptedText = encrypted.ciphertext.toString(CryptoJS.enc.Base64);
+    const ivBase64 = iv.toString(CryptoJS.enc.Base64);
+
+    // Return the base64 encoded IV and encrypted text
+    return `${ivBase64}:${encryptedText}`;
   };
 
 export {useConstructor,encryptTextResolver}
