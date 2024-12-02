e932c96104376cfae44c2bd6b9645315ad24e631
amir <amirdehghani137722@gmail.com>
2024-12-02 20:07:27 +0330

    add some encript

--- src/help.js
+++ src/help.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { useState } from "react";
 // import CryptoJS from 'crypto-js';
 
@@ -13,8 +14,15 @@ const useConstructor = (callBack = () => {}) => {
   const encryptTextResolver= (text) => {
     console.log(text)
     const secretKey = 'amir'
-    return CryptoJS.AES.encrypt(text.trim(), secretKey).toString();
-
+    // return CryptoJS.AES.encrypt(text.trim(), secretKey).toString();
+    const iv = CryptoJS.lib.WordArray.random(16); // Generate random IV
+    const encrypted = CryptoJS.AES.encrypt(text, CryptoJS.enc.Utf8.parse(secretKey), {
+      iv: iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+    });
+    // Return the IV and ciphertext concatenated
+    return iv.toString(CryptoJS.enc.Base64) + ':' + encrypted.toString();
   };
 
 export {useConstructor,encryptTextResolver}
