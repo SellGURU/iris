92b96d9e9a0267fb3fbe979ea166fd24873ea675
amir <amirdehghani137722@gmail.com>
2024-12-10 09:11:54 +0330

    update validations

--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -76,12 +76,7 @@ const Register = () => {
     password: Yup
       .string()
       .required('Password is required.')
-      .min(6, 'Password must be at least 6 characters')
-                .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-                .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-                .matches(/\d/, 'Password must contain at least one number')
-                .matches(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character')
-                .required('Password is required'),      
+      .min(6, 'Password must be at least 6 characters'),     
     confirm: Yup
       .string().required("Password is required.")
       .oneOf([Yup.ref('password')], 'Passwords must match'),      
