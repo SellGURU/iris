166137124be92a82ba819a9f1f78f7e124850306
amir <amirdehghani137722@gmail.com>
2024-12-18 20:19:40 +0330

    remove text jsutify

--- src/components/explation/index.jsx
+++ src/components/explation/index.jsx
@@ -12,13 +12,13 @@ const Explation = () => {
                 <div className="text-[#444444] text-[14px]">Results Explanation</div>
                 <img src="./info-circle-gray.svg " className="w-4 cursor-pointer" alt="" />
                 {isvisible &&
-                    <div className="w-[309px] text-[#444444]  text-justify absolute left-32 p-4 top-8 z-30 bg-white rounded-[8px] min-h-[167px]" style={{boxShadow:'0px 0px 6px 0px #00000026'}}>
+                    <div className="w-[309px] text-[#444444]  absolute left-32 p-4 top-8 z-30 bg-white rounded-[8px] min-h-[167px]" style={{boxShadow:'0px 0px 6px 0px #00000026'}}>
                        <div className="text-[14px] font-[600]"> Results Interpretation:</div>
                         <div className="text-[14px] ml-3 font-[600]">
-                           . Distances (mm):  <span className="font-[400]">Forehead Curve, Steiner's Line, Tail of Eyebrow Height, Slope of Eyebrow </span>
+                           . Distances (mm):  <span className="font-[400] w-full text-justify">Forehead Curve, Steiner's Line, Tail of Eyebrow Height, Slope of Eyebrow </span>
                         </div>
                         <div className="text-[14px] ml-3 font-[600]">
-                           . Ratios (no units): <span className="font-[400]"> All other results. </span>                       
+                           . Ratios (no units): <span className="font-[400] text-justify"> All other results. </span>                       
 
                         </div>
                     </div>
